7d8ebef3f8008b059bf7f6e3d2e49bcca091e00b
Alexander Pustovoy <alexanderpustovoy@Alex.local>
2017-06-07 12:45:16 +0300

    added new JS, css and HTML

---
+++ js/test.js
@@ -0,0 +1,47 @@
+var arrNames = ["Sania", "Sashka", "Alexander", "Jaja", "PES!"];
+
+function names(arrNames) {
+
+    var parentElement = document.body;
+    var ul = document.createElement('ul');
+    parentElement.appendChild(ul);
+
+    for (var i=0; i < arrNames.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = arrNames[i];
+        ul.appendChild(newLi);
+    }
+}
+names(arrNames);
+
+
+var t = performance.now();
+function uravnenie(a, b, c) {
+
+    var x1, x2;
+    
+    if(((a == 4)&&(b == 16)&&(c == 8)) || ((a == 5)&&(b == 10)&&(c == 5)) || ((a == -5)&&(b == 10)&&(c == -5)) || ((a == -1)&&(b == -1)&&(c == -2)) || ((a == 9)&&(b == 0)&&(c == -2)) || ((a == 21)&&(b == 23)&&(c == 4))) {
+        
+        //deskriminant
+        var d = Math.pow(b, 2) - 4*a*c;       
+        if (d<0) {
+            t = performance.now() - t;
+            console.log("Дискриминант ниже нуля - корней нет t=" + t);
+        } else if (d == 0) {
+            x1 = -b/(2*a);
+            t = performance.now() - t;
+            console.log("Для значений a=" + a + " b=" + b + " c=" + c + " результат n(x1=" + parseFloat(x1.toFixed(4)) + ") - t = " + t);
+        } else {
+            x1 = (-b+Math.sqrt(d))/(2*a);
+            x2 = (-b-Math.sqrt(d))/(2*a);
+            t = performance.now() - t;
+            console.log("Для значений a=" + a + " b=" + b + " c=" + c + " результат n(x1=" + parseFloat(x1.toFixed(4)) + ", x2=" + parseFloat(x2.toFixed(4)) + ") - t = " + t);
+        }
+
+    }
+
+    
+
+}
+
+uravnenie(-1, -1, -2);
